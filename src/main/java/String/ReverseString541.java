fa9d9351e995f45f85c2fbf5ac8f941038b6c030
fengliejv <1091892391@qq.com>
2017-09-09 00:30:36 +0800

    541

--- src/main/java/String/ReverseString541.java
+++ src/main/java/String/ReverseString541.java
@@ -6,7 +6,30 @@ package String;
 public class ReverseString541 {
     class Solution {
         public String reverseStr(String s, int k) {
-
+            StringBuilder s1;
+            StringBuilder s2;
+            StringBuilder s3;
+            StringBuilder s4;
+            StringBuilder sb = new StringBuilder();
+            int length = s.length();
+            int num = length/(2*k);
+            for(int i=0;i<num;i++){
+                s1 = new StringBuilder(s.substring(2*i*k,2*i*k+k));
+                sb.append(s1.reverse());
+                s2 = new StringBuilder(s.substring(2*i*k+k,2*i*k+2*k));
+                sb.append(s2);
+            }
+            int res = length%(2*k);
+            if(res<k){
+                 s3 = new StringBuilder(s.substring(length-res,length));
+                sb.append(s3.reverse());
+            }else {
+                 s3 = new StringBuilder(s.substring(length-res,length-res+k));
+                sb.append(s3.reverse());
+                 s4 = new StringBuilder(s.substring(length-res+k,length));
+                sb.append(s4);
+            }
+            return  sb.toString();
         }
     }
 }
