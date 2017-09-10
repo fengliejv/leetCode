1d1a5fc221e16d9742e380a072523f3a4c0dcb5e
fengliejv <1091892391@qq.com>
2017-09-10 23:04:19 +0800

    653 only copy

--- src/main/java/twosumIVInputisaBST653/TwoSumIVInput653.java
+++ src/main/java/twosumIVInputisaBST653/TwoSumIVInput653.java
@@ -1,18 +1,30 @@
 package twosumIVInputisaBST653;
 
+import tree.TreeNode;
+
+import java.util.HashMap;
+
 /**
  * Created by fengliejv on 2017/9/7.
  */
 public class TwoSumIVInput653 {
-    //todo
-//    class Solution {
-//        public boolean findTarget(TreeNode root, int k) {
-//
-//        }
-//
-//
-//
-//    }
+
+    class Solution {
+        HashMap<Integer,Boolean> hashMap = new HashMap<Integer, Boolean>();
+        public boolean findTarget(TreeNode root, int k) {
+            if(root==null){
+                return false;
+            }
+            if(hashMap.containsKey(k-root.val)){
+                return true;
+            }
+            hashMap.put(root.val,true);
+            return  findTarget(root.left,k)||findTarget(root.right,k);
+
+
+        }
+
+    }
 //
 //    public static void main(String[] args) {
 //
