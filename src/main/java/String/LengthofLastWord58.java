b4882567c7f49d906cbab09134bae442cd0e821d
fengliejv <1091892391@qq.com>
2017-09-17 11:30:09 +0800

    58

---
+++ src/main/java/String/LengthofLastWord58.java
@@ -0,0 +1,14 @@
+package String;
+
+/**
+ * Created by fengliejv on 2017/9/17.
+ */
+public class LengthofLastWord58 {
+    public int lengthOfLastWord(String s) {
+        String[] strings = s.split(" ");
+        if (strings.length == 0) {
+            return 0;
+        }
+        return strings[strings.length - 1].length();
+    }
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,10 +4,9 @@
 import String.*;
 public class Test {
     public static void main(String[] args) {
-        AddBinary67 jr = new AddBinary67();
+        LengthofLastWord58 jr = new LengthofLastWord58();
        // Solution solution = jr.new Solution();
-        System.out.println(jr.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101"
-                ,"110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
+        System.out.println(jr.lengthOfLastWord(""));
 
 
 
