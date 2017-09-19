8de9b8840b10f704f2e65229a17c52a5da07c01c
fengliejv <1091892391@qq.com>
2017-09-19 21:59:48 +0800

    14

--- src/main/java/String/LongestCommonPrefix14.java
+++ src/main/java/String/LongestCommonPrefix14.java
@@ -4,9 +4,25 @@ package String;
  * Created by fengliejv on 2017/9/16.
  */
 public class LongestCommonPrefix14 {
-    //todo
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0||strs[0].length()==0){
+            return "";
+        }
+        for(int i=0;i<strs[0].length();i++){
+            char ch = strs[0].charAt(i);
+            for(String string :strs){
+                if(string.length()-1<i){
+                    return string;
+                }
+                if(string.charAt(i)!=ch){
+                    return string.substring(0,i);
+                }
+            }
+        }
+        return strs[0];
+
+    }
 }
-//    public String longestCommonPrefix(String[] strs) {
-//
-//    }
-//}
+
+
