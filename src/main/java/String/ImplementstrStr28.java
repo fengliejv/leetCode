67da8e6fc156ab5cc9df5d30eaafe336223bf5ee
fengliejv <1091892391@qq.com>
2017-09-19 23:24:37 +0800

    28

--- src/main/java/String/ImplementstrStr28.java
+++ src/main/java/String/ImplementstrStr28.java
@@ -6,7 +6,29 @@ package String;
 //todo
 public class ImplementstrStr28 {
 
-//    public int strStr(String haystack, String needle) {
-//
-//    }
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0){
+            return 0;
+        }
+        if(haystack.length()==0||haystack.length()<needle.length()){
+            return -1;
+        }
+        for(int i=0;i<haystack.length()-needle.length()+1;i++){
+            int a=i;
+            for(int j=0;j<needle.length();j++){
+                if(haystack.charAt(a)!=needle.charAt(j)){
+                    break;
+                }
+                a++;
+                if(a==needle.length()-1){
+                    return i;
+                }
+                if(a==haystack.length()){
+                    return -1;
+                }
+            }
+
+        }
+        return -1;
+    }
 }
---
+++ src/main/java/String/RepeatedSubstringPattern459.java
@@ -0,0 +1,7 @@
+package String;
+
+/**
+ * Created by fengliejv on 2017/9/19.
+ */
+public class RepeatedSubstringPattern459 {
+}
