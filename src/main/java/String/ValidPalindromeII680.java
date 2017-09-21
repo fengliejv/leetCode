2b41f0f070637d89e7a9747d806b111c81469b1d
fengliejv <1091892391@qq.com>
2017-09-21 23:22:41 +0800

    125 what a fuck ! i only can copy it

---
+++ src/main/java/String/ValidPalindromeII680.java
@@ -0,0 +1,13 @@
+package String;
+
+/**
+ * Created by fengliejv on 2017/9/21.
+ */
+public class ValidPalindromeII680 {
+    public boolean validPalindrome(String s) {
+        String r = new StringBuilder(s).reverse().toString();
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+    }
+}
