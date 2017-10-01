62306ef115d74b4448097518a5c23b979ba8e969
fengliejv <1091892391@qq.com>
2017-10-02 01:54:50 +0800

    219

---
+++ src/main/java/array/ContainsDuplicateII219.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by fengliejv on 2017/10/2.
+ */
+
+public class ContainsDuplicateII219 {
+    //if use ArrayList ,will time out ,because of the binary search in it
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(k==0||nums.length==0){
+            return false;
+        }
+        //ArrayList arrayList = new ArrayList();
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if(i>k){
+                set.remove(nums[i-k-1]);
+            }
+            if(set.contains(nums[i])){
+                return true;
+            }else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+
+    }
+}
