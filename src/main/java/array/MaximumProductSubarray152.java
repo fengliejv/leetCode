029e2a46a0e7a8239015c9b2f4594ca3583f3fa5
fengliejv <1091892391@qq.com>
2017-10-02 01:26:58 +0800

    566

---
+++ src/main/java/array/MaximumProductSubarray152.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.ArrayList;
+
+/**
+ * Created by fengliejv on 2017/10/1.
+ */
+//todo
+public class MaximumProductSubarray152 {
+//    public int maxProduct(int[] nums) {
+//        if(nums.length==0){
+//            return 0;
+//        }
+//        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+//        int count=0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if(nums[i]>0)
+//        }
+}
--- src/main/java/array/ReshapetheMatrix566.java
+++ src/main/java/array/ReshapetheMatrix566.java
@@ -3,9 +3,24 @@ package array;
 /**
  * Created by fengliejv on 2017/10/1.
  */
-//todo
+
 public class ReshapetheMatrix566 {
-//    public int[][] matrixReshape(int[][] nums, int r, int c) {
-//
-//    }
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int num = nums.length*nums[0].length;
+        if(num<(r*(c-1)+1)||num>r*c){
+            return nums;
+        }
+        int[][] val = new int[r][c];
+        int m=0,n=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums[0].length;j++){
+                val[m][n++]=nums[i][j];
+                if(n==c){
+                    n=0;
+                    m++;
+                }
+            }
+        }
+        return val;
+    }
 }
