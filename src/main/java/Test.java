b19efcd82ba32a7c20e4a43aa202512c58a568b6
fengliejv <1091892391@qq.com>
2017-10-04 20:01:43 +0800

    204

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@
  */
 import String.*;
 import array.MaximumProductSubarray152;
+import math.CountPrimes204;
 import math.NthDigit400;
 import math.PalindromeNumber9;
 import math.Sqrt69;
@@ -13,9 +14,8 @@ import tree.TreeNode;
 
 public class Test {
     public static void main(String[] args) {
-        int[] a = new int[]{-4,-3,-2};
-        MaximumProductSubarray152 jr = new MaximumProductSubarray152();
-        System.out.println(jr.maxProduct(a));
+        CountPrimes204 jr = new CountPrimes204();
+        System.out.println(jr.countPrimes(499979));
 
 
 
--- src/main/java/math/CountPrimes204.java
+++ src/main/java/math/CountPrimes204.java
@@ -5,7 +5,28 @@ package math;
  */
 //todo
 public class CountPrimes204 {
-//    public int countPrimes(int n) {
-//
-//    }
+    public int countPrimes(int n) {
+        if(n<2){
+            return 0;
+        }
+        int count =0;
+        int[] bitmap = new int[n];
+        bitmap[0]=1;
+        bitmap[1]=1;
+//        bitmap[2]=0;
+        for (int i = 2; i < n; i++) {
+            int max = n%i==0?n/i:n/i+1;
+            for(int j=i;j<max;j++){
+                if(bitmap[i*j]!=1){
+                    bitmap[i*j]=1;
+                }
+            }
+        }
+        for(int i:bitmap){
+            if(i==0){
+                count++;
+            }
+        }
+        return count;
+    }
 }
