9d627a281ff9afa07d3f2a42fbc6a2086f722b56
fengliejv <1091892391@qq.com>
2017-10-07 22:25:42 +0800

    257

--- src/main/java/tree/BinaryTreePaths257.java
+++ src/main/java/tree/BinaryTreePaths257.java
@@ -7,23 +7,37 @@ import java.util.List;
 /**
  * Created by fengliejv on 2017/9/23.
  */
-//todo
+
 public class BinaryTreePaths257 {
-//    List<String> list = new ArrayList<String>();
-//    StringBuilder sb = new StringBuilder();
-//
-//    public List<String> binaryTreePaths(TreeNode root) {
-//        if(root==null){
-//            return null;
-//        }
-//        if(root.left==null&&root.right==null){
-//            return list;
-//        }
-//
-//        if(root.left!=null){
-//            return binaryTreePaths(root.left);
-//        }
-//
-//
-//    }
+    List<String> list = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        addString(root, new String());
+        return list;
+    }
+
+    private void addString(TreeNode treeNode, String string) {
+        if(treeNode==null){
+            return;
+        }
+        string = string + treeNode.val + "";
+
+        if (treeNode.left == null&&treeNode.right==null) {
+            list.add(string);
+            return;
+        }
+        if(treeNode.left!=null){
+
+            addString(treeNode.left, string+"->" );
+        }
+        if(treeNode.right!=null){
+
+            addString(treeNode.right, string+"->" );
+        }
+    }
+
+
 }
