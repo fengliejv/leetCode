5d8cb2125d0ef52f9ed3c0a9e9ddf87b22d7485a
fengliejv <1091892391@qq.com>
2017-10-07 17:20:50 +0800

    110

--- src/main/java/math/PalindromeLinkedList234.java
+++ src/main/java/math/PalindromeLinkedList234.java
@@ -6,6 +6,11 @@ package math;
 //todo
 public class PalindromeLinkedList234 {
 //    public boolean isPalindrome(ListNode head) {
-//
+//        ListNode quick = head;
+//        ListNode slow = head;
+//        while (quick.next!=null){
+//            quick = quick.next.next;
+//            slow = slow.next;
+//        }
 //    }
 }
--- src/main/java/tree/BalancedBinaryTree110.java
+++ src/main/java/tree/BalancedBinaryTree110.java
@@ -3,30 +3,55 @@ package tree;
 /**
  * Created by fengliejv on 2017/9/23.
  */
-// top down approach  todo: dfs o(n)
+// top down approach  and dfs,dfs need some skills
 public class BalancedBinaryTree110 {
-    public boolean isBalanced(TreeNode root) {
+    public boolean isBalancedDFS(TreeNode root) {
         if(root==null){
             return true;
         }
-        if(!isBalance(root.left,root.right)){
-            return false;
+
+        return dept(root)!=Integer.MAX_VALUE;
+    }
+    private int dept(TreeNode root){
+        int leftDepth = 0;
+        int rightDepth = 0;
+        if(root.left!=null){
+            leftDepth = dept(root.left);
         }
-        return isBalanced(root.left)&&isBalanced(root.right);
+        if(root.right!=null){
+            rightDepth = dept(root.right);
+        }
+        if(leftDepth==Integer.MAX_VALUE||rightDepth==Integer.MAX_VALUE||Math.abs(leftDepth-rightDepth)>1){
+            return Integer.MAX_VALUE;
+        }
+        return Math.max(leftDepth,rightDepth)+1;
+
     }
 
-    private boolean isBalance(TreeNode l,TreeNode r){
-        if(Math.abs(depth(l)-depth(r))<2){
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
             return true;
-        }else {
+        }
+        if (!isBalance(root.left, root.right)) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private boolean isBalance(TreeNode l, TreeNode r) {
+        if (Math.abs(depth(l) - depth(r)) < 2) {
+            return true;
+        } else {
             return false;
         }
     }
-    private int depth(TreeNode treeNode){
-        if(treeNode==null){
+
+    private int depth(TreeNode treeNode) {
+        if (treeNode == null) {
             return 0;
         }
-        return 1+Math.max(depth(treeNode.left),depth(treeNode.right));
+        return 1 + Math.max(depth(treeNode.left), depth(treeNode.right));
     }
 
 }
