d5355d952805a0f7e238f5f4062882e76c0c05d1
fengliejv <1091892391@qq.com>
2017-10-09 21:26:02 +0800

    543

--- src/main/java/tree/DiameterofBinaryTree543.java
+++ src/main/java/tree/DiameterofBinaryTree543.java
@@ -3,9 +3,31 @@ package tree;
 /**
  * Created by fengliejv on 2017/9/23.
  */
-//todo
+
 public class DiameterofBinaryTree543 {
-//    public int diameterOfBinaryTree(TreeNode root) {
-//
-//    }
+    int max;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter(root);
+        return max;
+    }
+    private int diameter(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        //int count=0;
+        int left = diameter(node.left);
+        int right = diameter(node.right);
+        if(node.left!=null){
+            left++;
+        }else {
+            left=0;
+        }
+        if(node.right!=null){
+            right++;
+        }else {
+            right=0;
+        }
+        max=right+left>max?right+left:max;
+        return Math.max(left,right);
+    }
 }
