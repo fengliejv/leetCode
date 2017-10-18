99d65ee7720679a048244110ad49755b514a969e
fengliejv <1091892391@qq.com>
2017-10-18 22:37:28 +0800

    560

--- src/main/java/array/SubarraySumEqualsK560.java
+++ src/main/java/array/SubarraySumEqualsK560.java
@@ -1,8 +1,28 @@
 package array;
 
+import java.util.HashMap;
+
 /**
  * Created by fengliejv on 2017/10/17.
  */
-//todo
+
 public class SubarraySumEqualsK560 {
+
+
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            if (map.containsKey(sum-k)) {
+                count = count + map.get(sum - k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+
+        return count;
+    }
 }
