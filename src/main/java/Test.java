584eefce2b488f959cf78fd7a14b01575f71b5ea
fengliejv <1091892391@qq.com>
2017-10-24 22:46:19 +0800

    215 with two solution . the first is using java API and the second is using the partition

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@
  * Created by fengliejv on 2017/9/11.
  */
 import array.ImageSmoother661;
+import array.KthLargestElementinanArray215;
 import array.PascalsTriangleII119;
 import map.MaxAreaofIsland695;
 import map.SurroundedRegions130;
@@ -16,14 +17,22 @@ import tree.*;
 public class Test {
     public static void main(String[] args) {
         int[][] grifd= new int[][]{{1,1,1},{1,0,1},{1,1,1}};
-        int[] a = new int[]{3,5};
+        int[] a = new int[]{5,2,4,1,3,6,0,2};
+        int[] b = new int[]{-1,2,0};
+        int[] c = new int[]{4,3,2,1};
+
         ListNode node = new ListNode();
         ListNode n = node.init(a);
         String s ="cbaebabacd";
         String t ="abc";
         //TreeNode node = TreeNode.createBinTree(new int[]{1,4,5,4,4,5});
-        PascalsTriangleII119 jr = new PascalsTriangleII119();
-        System.out.println(jr.getRow(3));
+        KthLargestElementinanArray215 jr = new KthLargestElementinanArray215();
+        System.out.println(jr.findKthLargest(a,2));
+        System.out.println(jr.findKthLargest(a,1));
+        System.out.println(jr.findKthLargest(a,8));
+        System.out.println(jr.findKthLargest(b,1));
+        System.out.println(jr.findKthLargest(c,3));
+
 
 
 
--- src/main/java/array/KthLargestElementinanArray215.java
+++ src/main/java/array/KthLargestElementinanArray215.java
@@ -6,11 +6,58 @@ import java.util.Arrays;
  * Created by fengliejv on 2017/10/23.
  */
 public class KthLargestElementinanArray215 {
-
+//todo
+// i think the way use partition is better
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        int low = 0;
+        int high = nums.length-1;
+
+        int pos = nums.length-k;
+        int i ;
+        while (true) {
+             i = partition(low, high, nums);
+            if(i==pos){
+                break;
+            }else if(i<pos){
+                low=i+1;
+            }else if(i>pos){
+                high=i-1;
+            }
+        }
+        return nums[pos];
+
     }
+    private int partition(int low ,int high,int[] nums){
+        if(low==high){
+            return low;
+        }
+        int cmp = nums[low];
+        int k = low;
+        while (high>low) {
+            while (high > low && nums[high] >= cmp) {
+                high--;
+            }
+            int i = nums[high];
+            nums[high] = nums[k];
+            nums[k] = i;
+            k=high;
+            while ((low < high && nums[low] < cmp)) {
+                low++;
+            }
+            i = nums[low];
+            nums[low] = nums[k];
+            nums[k] = i;
+            k=low;
+            if(low==high){
+                break;
+            }
+
+        }
+
+        return low;
+
+    }
+
 
     //although this beats 78.55 i think this is not enough
     public int findKthLargest1(int[] nums, int k) {
