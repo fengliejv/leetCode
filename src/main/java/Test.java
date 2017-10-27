a6cf7ab18490e328d64eae707a8ac54e9cd498e3
fengliejv <1091892391@qq.com>
2017-10-27 21:20:02 +0800

    389 a XOR method is better

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -16,23 +16,28 @@ import tree.*;
 
 public class Test {
     public static void main(String[] args) {
-        int[][] grifd= new int[][]{{1,1,1},{1,0,1},{1,1,1}};
-        int[] a = new int[]{5,2,4,1,3,6,0,2};
-        int[] b = new int[]{-1,2,0};
-        int[] c = new int[]{4,3,2,1};
-
-        ListNode node = new ListNode();
-        ListNode n = node.init(a);
-        String s ="cbaebabacd";
-        String t ="abc";
-        //TreeNode node = TreeNode.createBinTree(new int[]{1,4,5,4,4,5});
-        KthLargestElementinanArray215 jr = new KthLargestElementinanArray215();
-        System.out.println(jr.findKthLargest(a,2));
-        System.out.println(jr.findKthLargest(a,1));
-        System.out.println(jr.findKthLargest(a,8));
-        System.out.println(jr.findKthLargest(b,1));
-        System.out.println(jr.findKthLargest(c,3));
-
+//        int[][] grifd= new int[][]{{1,1,1},{1,0,1},{1,1,1}};
+//        int[] a = new int[]{5,2,4,1,3,6,0,2};
+//        int[] b = new int[]{-1,2,0};
+//        int[] c = new int[]{4,3,2,1};
+//
+//        ListNode node = new ListNode();
+//        ListNode n = node.init(a);
+//        String s ="cbaebabacd";
+//        String t ="abc";
+//        //TreeNode node = TreeNode.createBinTree(new int[]{1,4,5,4,4,5});
+//        KthLargestElementinanArray215 jr = new KthLargestElementinanArray215();
+//        System.out.println(jr.findKthLargest(a,2));
+//        System.out.println(jr.findKthLargest(a,1));
+//        System.out.println(jr.findKthLargest(a,8));
+//        System.out.println(jr.findKthLargest(b,1));
+//        System.out.println(jr.findKthLargest(c,3));
+        Integer i = 1;
+        if(i==1){
+            System.out.println("OK");
+        }else {
+            System.out.println("hehe");
+        }
 
 
 
--- src/main/java/map/FindtheDifference389.java
+++ src/main/java/map/FindtheDifference389.java
@@ -26,4 +26,13 @@ public class FindtheDifference389 {
         }
         return ' ';
     }
+    public char findTheDifference1(String s, String t) {
+        int n = t.length();
+        char c = t.charAt(n - 1);
+        for (int i = 0; i < n - 1; ++i) {
+            c ^= s.charAt(i);
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
 }
