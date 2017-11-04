c3e7369590633923bc965e1adefaee8cc072ce26
fengliejv <1091892391@qq.com>
2017-11-04 22:54:44 +0800

    581

--- src/main/java/array/ShortestUnsortedContinuousSubarray581.java
+++ src/main/java/array/ShortestUnsortedContinuousSubarray581.java
@@ -3,9 +3,27 @@ package array;
 /**
  * Created by fengliejv on 2017/10/29.
  */
-//todo
 public class ShortestUnsortedContinuousSubarray581 {
-//    public int findUnsortedSubarray(int[] nums) {
-//
-//    }
+    public int findUnsortedSubarray(int[] nums) {
+        int end = 0;
+        int begin = nums.length-1;
+        int num = nums[0];
+        for (int i = 0; i <nums.length ; i++) {
+            if(nums[i]<num){
+                end=i;
+            }else {
+                num = nums[i];
+            }
+        }
+        num=nums[nums.length-1];
+        for (int i = nums.length-1; i >=0 ; i--) {
+            if(nums[i]>num){
+                begin = i;
+            }else {
+                num=nums[i];
+            }
+        }
+        
+        return  end>begin?end-begin+1:0;
+    }
 }
