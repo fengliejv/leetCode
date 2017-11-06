8568f2f4b299109614c9c4f20a70d3a728954a03
fengliejv <1091892391@qq.com>
2017-11-06 21:13:52 +0800

    235 an amazing solution

--- src/main/java/tree/LowestCommonAncestorofaBinarySearchTree235.java
+++ src/main/java/tree/LowestCommonAncestorofaBinarySearchTree235.java
@@ -20,4 +20,9 @@ public class LowestCommonAncestorofaBinarySearchTree235 {
             return lowestCommonAncestor(root.right,p,q);
         }
     }
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        while ((root.val - p.val) * (root.val - q.val) > 0)
+            root = p.val < root.val ? root.left : root.right;
+        return root;
+    }
 }
