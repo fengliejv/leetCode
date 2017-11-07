5166cdb7c21d572a6e30160c908875174ce488db
fengliejv <1091892391@qq.com>
2017-11-07 22:57:40 +0800

    501

--- src/main/java/math/PowerofFour342.java
+++ src/main/java/math/PowerofFour342.java
@@ -4,25 +4,25 @@ package math;
  * Created by fengliejv on 2017/11/7.
  */
 public class PowerofFour342 {
-    public boolean isPowerOfFour(int num) {
+    public boolean isPowerOfFour2(int num) {
         return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
 
     }
-//    public boolean isPowerOfFour(int num) {
-//        if (num < 0) {
-//            return false;
-//        }
-//        String str = Integer.toBinaryString(num);
-//        if (str.charAt(0) != '1') {
-//            return false;
-//        }
-//        if (str.indexOf("1", 1) > 0) {
-//            return false;
-//        }
-//
-//        int lenth = str.length() - 1;
-//        return lenth % 2 == 0;
-//
-//    }
+    public boolean isPowerOfFour(int num) {
+        if (num < 0) {
+            return false;
+        }
+        String str = Integer.toBinaryString(num);
+        if (str.charAt(0) != '1') {
+            return false;
+        }
+        if (str.indexOf("1", 1) > 0) {
+            return false;
+        }
+
+        int lenth = str.length() - 1;
+        return lenth % 2 == 0;
+
+    }
 
 }
--- src/main/java/tree/FindModeinBinarySearchTree501.java
+++ src/main/java/tree/FindModeinBinarySearchTree501.java
@@ -1,8 +1,58 @@
 package tree;
 
+import java.util.ArrayList;
+
 /**
  * Created by fengliejv on 2017/11/6.
  */
-//todo
 public class FindModeinBinarySearchTree501 {
+    Integer pre = null;
+    int count = 0;
+    int max = Integer.MIN_VALUE;
+
+    public int[] findMode(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        search(root, arrayList);
+        int[] res = new int[arrayList.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = arrayList.get(i);
+        }
+        return res;
+    }
+
+    private void search(TreeNode root, ArrayList list) {
+        if(root==null){
+            return;
+        }
+        search(root.left,list);
+        if(pre==null){
+            pre=root.val;
+            count=1;
+            max=1;
+            list.add(root.val);
+        }else {
+            if(root.val==pre){
+                count++;
+                if(count==max){
+                    list.add(root.val);
+                }
+                if(count>max){
+                    list.clear();
+                    list.add(root.val);
+                    max=count;
+                }
+            }else {
+                count=1;
+                pre=root.val;
+                if(count==max){
+                    list.add(root.val);
+                }
+            }
+        }
+
+        search(root.right,list);
+    }
 }
