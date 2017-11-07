dfd3a4f13c76299d2ab2d12a973a67eeb959b399
fengliejv <1091892391@qq.com>
2017-11-07 22:24:41 +0800

    342  a one line solution

--- src/main/java/math/PowerofFour342.java
+++ src/main/java/math/PowerofFour342.java
@@ -5,20 +5,24 @@ package math;
  */
 public class PowerofFour342 {
     public boolean isPowerOfFour(int num) {
-        if (num < 0) {
-            return false;
-        }
-        String str = Integer.toBinaryString(num);
-        if (str.charAt(0) != '1') {
-            return false;
-        }
-        if (str.indexOf("1", 1) > 0) {
-            return false;
-        }
-
-        int lenth = str.length() - 1;
-        return lenth % 2 == 0;
+        return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
 
     }
+//    public boolean isPowerOfFour(int num) {
+//        if (num < 0) {
+//            return false;
+//        }
+//        String str = Integer.toBinaryString(num);
+//        if (str.charAt(0) != '1') {
+//            return false;
+//        }
+//        if (str.indexOf("1", 1) > 0) {
+//            return false;
+//        }
+//
+//        int lenth = str.length() - 1;
+//        return lenth % 2 == 0;
+//
+//    }
 
 }
