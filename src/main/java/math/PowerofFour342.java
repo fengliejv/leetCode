00477c4b7ec9926f442edc86ec7c88b4bb21d119
fengliejv <1091892391@qq.com>
2017-11-07 22:21:04 +0800

    342

---
+++ src/main/java/math/PowerofFour342.java
@@ -0,0 +1,24 @@
+package math;
+
+/**
+ * Created by fengliejv on 2017/11/7.
+ */
+public class PowerofFour342 {
+    public boolean isPowerOfFour(int num) {
+        if (num < 0) {
+            return false;
+        }
+        String str = Integer.toBinaryString(num);
+        if (str.charAt(0) != '1') {
+            return false;
+        }
+        if (str.indexOf("1", 1) > 0) {
+            return false;
+        }
+
+        int lenth = str.length() - 1;
+        return lenth % 2 == 0;
+
+    }
+
+}
