0fe195a5f8e566b4984cfc003242caa1b4eaebe2
fengliejv <1091892391@qq.com>
2017-11-08 21:08:04 +0800

    669

--- src/main/java/tree/TrimaBinarySearchTree669.java
+++ src/main/java/tree/TrimaBinarySearchTree669.java
@@ -3,44 +3,40 @@ package tree;
 /**
  * Created by fengliejv on 2017/11/5.
  */
-//todo
-//public class TrimaBinarySearchTree669 {
-//    int low=0;
-//    int high =0;
-//    public TreeNode trimBST(TreeNode root, int L, int R) {
-//        low=L;
-//        high=R;
-//        while (root.val>high){
-//            if(root.left!=null){
-//                root=root.left;
-//            }else {
-//                return null;
-//            }
-//        }
-//
-//        while (root.val<low){
-//            if(root.right!=null){
-//                root = root.right;
-//            }else {
-//                return null;
-//            }
-//        }
-//        trim(root);
-//        return root;
-//    }
-//
-//    private void trim(TreeNode node){
-//        if(node==null){
-//            return;
-//        }
-//        if(node.left!=null&&node.left.val<low){
-//
-//            node.left=node.left.right;
-//        }
-//        if(node.right!=null&&node.right.val>high){
-//            node.right = node.right.left;
-//        }
-//        trim(node.left);
-//        trim(node.right);
-////    }
-//}
+
+public class TrimaBinarySearchTree669 {
+    int low=0;
+    int high =0;
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        low=L;
+        high=R;
+        while (root.val>high||root.val<low){
+            if(root.val>high&&root.left!=null){
+                root=root.left;
+            }else if(root.val<low&&root.right!=null){
+                root = root.right;
+            }else {
+                return null;
+            }
+        }
+        trim(root);
+        return root;
+    }
+
+    private void trim(TreeNode node){
+        if(node==null){
+            return;
+        }
+        if(node.left!=null&&node.left.val<low){
+
+            node.left=node.left.right;
+            trim(node);
+        }
+        if(node.right!=null&&node.right.val>high){
+            node.right = node.right.left;
+            trim(node);
+        }
+        trim(node.left);
+        trim(node.right);
+    }
+}
