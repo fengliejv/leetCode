87caf97a68bda2fb439acfebc92d51a6065aaedb
fengliejv <1091892391@qq.com>
2017-11-09 23:05:16 +0800

    190 this seems my work in HuaWei

---
+++ src/main/java/math/SumofTwoIntegers371.java
@@ -0,0 +1,11 @@
+package math;
+
+/**
+ * Created by fengliejv on 2017/11/9.
+ */
+public class SumofTwoIntegers371 {
+    //todo to see the discuss ,important
+//    public int getSum(int a, int b) {
+//
+//    }
+}
