42940ea052016faed68ba01fc39a71fa7e032d21
fengliejv <1091892391@qq.com>
2017-11-13 22:26:18 +0800

    371

--- src/main/java/math/SumofTwoIntegers371.java
+++ src/main/java/math/SumofTwoIntegers371.java
@@ -4,8 +4,14 @@ package math;
  * Created by fengliejv on 2017/11/9.
  */
 public class SumofTwoIntegers371 {
-    //todo to see the discuss ,important
-//    public int getSum(int a, int b) {
-//
-//    }
+    public int getSum(int a, int b) {
+        if(b==0){
+            return a;
+        }
+        int sum,carry;
+        sum = a^b;
+        carry=(a&b)<<1;
+        return getSum(sum,carry);
+
+    }
 }
