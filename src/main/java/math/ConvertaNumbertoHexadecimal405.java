5b6cd4e86f8effc09fe27a070d1dc3335c75991c
fengliejv <1091892391@qq.com>
2017-11-13 23:10:05 +0800

    405

---
+++ src/main/java/math/ConvertaNumbertoHexadecimal405.java
@@ -0,0 +1,11 @@
+package math;
+
+/**
+ * Created by fengliejv on 2017/11/13.
+ */
+public class ConvertaNumbertoHexadecimal405 {
+    public String toHex(int num) {
+        return Integer.toHexString(num);
+
+    }
+}
