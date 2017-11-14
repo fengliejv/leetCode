5c86d2304ff8340f706aae5f9ee2d0e35ece9c66
fengliejv <1091892391@qq.com>
2017-11-14 23:03:18 +0800

    504

---
+++ src/main/java/math/Base7504.java
@@ -0,0 +1,10 @@
+package math;
+
+/**
+ * Created by fengliejv on 2017/11/14.
+ */
+public class Base7504 {
+    public String convertToBase7(int num) {
+        return Integer.toString(num,7);
+    }
+}
