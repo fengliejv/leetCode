e2492890a7199454d31127da2901da6febebbef0
fengliejv <1091892391@qq.com>
2017-11-18 00:11:27 +0800

    11 a more eff way

--- src/main/java/array/ContainerWithMostWater11.java
+++ src/main/java/array/ContainerWithMostWater11.java
@@ -4,9 +4,26 @@ package array;
  * Created by fengliejv on 2017/11/17.
  */
 public class ContainerWithMostWater11 {
-//    public int maxArea(int[] height) {
-//
-//    }
+    public int maxArea(int[] height) {
+        if(height.length<=1){
+            return 0;
+        }
+        int begin = 0;
+        int end = height.length-1;
+        int max=0;
+        while (begin<end){
+            int area = (end-begin)*Math.min(height[begin],height[end]);
+            if(area>max){
+                max=area;
+            }
+            if(height[begin]>height[end]){
+                end--;
+            }else {
+                begin++;
+            }
+        }
+        return max;
+    }
 
     public int maxArea1(int[] height) {
         int lenth = height.length-1;
