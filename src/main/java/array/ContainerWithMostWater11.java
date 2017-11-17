8fc603effd7427c55a4cb3baa0b0a85dc458218a
fengliejv <1091892391@qq.com>
2017-11-17 23:43:53 +0800

    11 this is a stupid solution, i will find a better one

---
+++ src/main/java/array/ContainerWithMostWater11.java
@@ -0,0 +1,22 @@
+package array;
+
+/**
+ * Created by fengliejv on 2017/11/17.
+ */
+public class ContainerWithMostWater11 {
+
+
+    public int maxArea1(int[] height) {
+        int lenth = height.length-1;
+        int maxArea = 0;
+        for (int i = 1; i <=lenth; i++) {
+            for (int j = 0; j <=lenth-i ; j++) {
+                int area = i*Math.min(height[j],height[j+i]);
+                if(area>maxArea){
+                    maxArea=area;
+                }
+            }
+        }
+        return maxArea;
+    }
+}
