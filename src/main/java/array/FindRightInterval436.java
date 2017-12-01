64d678ac0e9e1547614788f67e7870e516a4fd76
fengliejv <1091892391@qq.com>
2017-12-01 18:43:43 +0800

    436

--- src/main/java/array/FindRightInterval436.java
+++ src/main/java/array/FindRightInterval436.java
@@ -1,11 +1,23 @@
 package array;
 
+import java.util.TreeMap;
+
 /**
  * Created by fengliejv on 2017/11/30.
  */
-//todo
+
 public class FindRightInterval436 {
-//    public int[] findRightInterval(Interval[] intervals) {
-//
-//    }
+    public int[] findRightInterval(Interval[] intervals) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        int[] res = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            map.put(intervals[i].start,i);
+        }
+        for (int i = 0; i < intervals.length; i++) {
+            Integer key = map.ceilingKey(intervals[i].end);
+            res[i]=key==null?-1:map.get(key);
+        }
+        return res;
+
+    }
 }
