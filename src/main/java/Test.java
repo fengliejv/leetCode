93498b0b4dc6947a866461a642db6d91d213d880
fengliejv <1091892391@qq.com>
2017-12-03 23:29:27 +0800

    240

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,7 @@
  * Created by fengliejv on 2017/9/11.
  */
 import array.DungeonGame174;
+import array.Searcha2DMatrixII240;
 import math.TrappingRainWater42;
 import tree.*;
 //import tree.Bit
@@ -14,6 +15,9 @@ public class Test {
                 {-5,-10,1},
                 {10,30,-5}
                                     };
+        int[][] g= new int[][]{{-1},{-1},
+
+        };
         int[] a = new int[]{2,0,2};
 //        int[] b = new int[]{-1,2,0};
 //        int[] c = new int[]{4,3,2,1};
@@ -23,9 +27,9 @@ public class Test {
 //        String s ="cbaebabacd";
 //        String t ="abc";
         TreeNode node = TreeNode.createBinTree(new int[]{1,0,2});
-        DungeonGame174 jr = new DungeonGame174();
+        Searcha2DMatrixII240 jr = new Searcha2DMatrixII240();
 
-        System.out.println( jr.calculateMinimumHP(grifd));
+        System.out.println( jr.searchMatrix(g,0));
 
 
 
--- src/main/java/array/Searcha2DMatrixII240.java
+++ src/main/java/array/Searcha2DMatrixII240.java
@@ -3,6 +3,27 @@ package array;
 /**
  * Created by fengliejv on 2017/12/1.
  */
-//todo
+
 public class Searcha2DMatrixII240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length < 1) {
+            return false;
+        }
+        int x = matrix.length;
+        int y = matrix[0].length;
+        for (int i = y-1; i >= 0; i--) {
+            if (matrix[0][i] == target) {
+                return true;
+            }
+            if (matrix[0][i] < target) {
+                for (int j = 1; j < x; j++) {
+                    if(matrix[j][i]==target){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
 }
