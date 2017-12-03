75fc4c78c49ce3e68700747601a071dbf3fdebc8
fengliejv <1091892391@qq.com>
2017-12-03 23:32:28 +0800

    240 a more eff

--- src/main/java/array/Searcha2DMatrixII240.java
+++ src/main/java/array/Searcha2DMatrixII240.java
@@ -26,4 +26,22 @@ public class Searcha2DMatrixII240 {
         return false;
     }
 
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
+            return false;
+        }
+        int col = matrix[0].length-1;
+        int row = 0;
+        while(col >= 0 && row <= matrix.length-1) {
+            if(target == matrix[row][col]) {
+                return true;
+            } else if(target < matrix[row][col]) {
+                col--;
+            } else if(target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+
 }
