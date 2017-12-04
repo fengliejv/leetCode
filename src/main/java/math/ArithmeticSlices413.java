7b59bad12f824623c902204f916361ddfae7ba19
fengliejv <1091892391@qq.com>
2017-12-04 22:03:43 +0800

    413

--- src/main/java/math/ArithmeticSlices413.java
+++ src/main/java/math/ArithmeticSlices413.java
@@ -3,9 +3,24 @@ package math;
 /**
  * Created by fengliejv on 2017/12/4.
  */
-//todo
 public class ArithmeticSlices413 {
-//    public int numberOfArithmeticSlices(int[] A) {
-//
-//    }
+    public int numberOfArithmeticSlices(int[] A) {
+        int sum=0;
+        if(A.length<3)
+            return 0;
+        int count =1;
+        int pre = A[1]-A[0];
+        for (int i = 2; i < A.length; i++) {
+            int val = A[i]-A[i-1];
+            if(val==pre){
+                count++;
+            }else {
+                sum+=(count)*(count-1)/2;
+                count=1;
+                pre=val;
+            }
+        }
+        sum+=(count)*(count-1)/2;
+        return sum;
+    }
 }
