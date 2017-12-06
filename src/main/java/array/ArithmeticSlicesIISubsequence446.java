9ed4683e7f6175f337425919154f6588a82a97b6
fengliejv <1091892391@qq.com>
2017-12-06 21:06:31 +0800

    682

---
+++ src/main/java/array/ArithmeticSlicesIISubsequence446.java
@@ -0,0 +1,8 @@
+package array;
+
+/**
+ * Created by fengliejv on 2017/12/4.
+ */
+//todo
+public class ArithmeticSlicesIISubsequence446 {
+}
---
+++ src/main/java/array/BaseballGame682.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.LinkedList;
+
+/**
+ * Created by fengliejv on 2017/12/6.
+ */
+public class BaseballGame682 {
+    public int calPoints(String[] ops) {
+        int sum = 0;
+        LinkedList<Integer> list = new LinkedList<>();
+        for (String op : ops) {
+            if (op.equals("C")) {
+                sum -= list.removeLast();
+            }
+            else if (op.equals("D")) {
+                list.add(list.peekLast() * 2);
+                sum += list.peekLast();
+            }
+            else if (op.equals("+")) {
+                list.add(list.peekLast() + list.get(list.size() - 2));
+                sum += list.peekLast();
+            }
+            else {
+                list.add(Integer.parseInt(op));
+                sum += list.peekLast();
+            }
+        }
+        return sum;
+    }
+}
