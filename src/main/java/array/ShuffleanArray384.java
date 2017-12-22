d7b53501110ea0c50ff182950fd36390cc3c203d
fengliejv <1091892391@qq.com>
2017-12-22 23:55:37 +0800

    384

---
+++ src/main/java/array/ShuffleanArray384.java
@@ -0,0 +1,28 @@
+package array;
+
+/**
+ * Created by fengliejv on 2017/12/22.
+ */
+public class ShuffleanArray384 {
+    private int[] nums;
+
+    public ShuffleanArray384(int[] nums) {
+        this.nums = nums;
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] rand = new int[nums.length];
+        for (int i = 0; i < nums.length; i++){
+            int r = (int) (Math.random() * (i+1));
+            rand[i] = rand[r];
+            rand[r] = nums[i];
+        }
+        return rand;
+    }
+}
---
+++ src/main/java/math/RangeSumQuery2DImmutable304.java
@@ -0,0 +1,8 @@
+package math;
+
+/**
+ * Created by fengliejv on 2017/12/22.
+ */
+//todo
+public class RangeSumQuery2DImmutable304 {
+}
