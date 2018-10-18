810ccd2a4c0ccb6e9f703eae0417813ad1d808bd
fengliejv <1091892391@qq.com>
2018-10-18 10:32:52 +0800

    142

---
+++ src/main/java/array/LinkedListCycleII142.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by fengliejv on 2018/10/18.
+ */
+
+public class LinkedListCycleII142 {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> nodeSet = new HashSet<>();
+        while (head!=null){
+            if(nodeSet.contains(head)){
+               return head;
+            }else {
+                nodeSet.add(head);
+                head=head.next;
+            }
+        }
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
